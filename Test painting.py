67f6a2864cea5ddd0aed64243d543fb6742b1629
VitaliyDzhabrailov <vitalyadd22000022@gmail.com>
2023-02-10 16:28:51 +0300

    Test painting timer works with time_sleep

--- Test painting.py
+++ Test painting.py
@@ -3,25 +3,33 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 from PyQt5.QtGui import QPainter, QBrush, QPen
 from PyQt5.QtCore import Qt, QTimer
+import time
 
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.timer = QTimer()
+        self.timer.startTimer(1000)
         self.title = "PyQt5 Drawing Tutorial"
         self.top= 150
         self.left= 150
         self.width = 500
         self.height = 500
-        self.timer = QTimer()
         self.init_window()
         self.x_0 = 10
 
     def init_window(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.top, self.left, self.width, self.height)
-        self.timer.start(1000)
         self.show()
+
+    # def timerEvent(self, event):
+    #     if event.timerId() == self.timer.timerId():
+    #         self.update()
+    #     else:
+    #         super(Window, self).timerEvent(event)
+    #         self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
@@ -32,8 +40,9 @@ class Window(QMainWindow):
         self.x_0 += 10
         x = self.x_0
         painter.drawEllipse(x, x, 40, 40)
+        # time.sleep(1)
+        # self.update()
         print(x)
-        self.update()
 
 
 if __name__ == '__main__':
