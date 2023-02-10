81d18beb89ce395994efa36973c9a4a0b2484404
Square596 <artem211201@gmail.com>
2023-02-10 23:29:32 +0300

    grid, cell

--- v2.py
+++ v2.py
@@ -10,10 +10,13 @@ class MyWindow(object):
 
         self.central_widget = QtWidgets.QWidget(MainWindow)
 
-        self.game_widget = Board(self.central_widget)
-        self.game_widget.setGeometry(QtCore.QRect(0, 0, 800, 800))
-        self.game_widget.setAttribute(QtCore.Qt.WA_StyledBackground)
-        self.game_widget.setStyleSheet('background-color: rgb(134, 230, 141);')
+        self.grid = Grid(self.central_widget)
+        self.grid.setGeometry(QtCore.QRect(0, 0, ImageWidget.size[0]+1, ImageWidget.size[1]+1))
+        self.grid.setAttribute(QtCore.Qt.WA_StyledBackground)
+        self.grid.setStyleSheet('background-color: rgb(134, 230, 141);')
+
+        self.game_widget = ImageWidget(self.grid)
+        self.game_widget.setGeometry(QtCore.QRect(0, 0, ImageWidget.size[0]+1, ImageWidget.size[1]+1))
 
         self.play_pause_btn = QtWidgets.QPushButton(self.central_widget)
         self.play_pause_btn.setGeometry(QtCore.QRect(850, 670, 200, 80))
@@ -27,22 +30,44 @@ class MyWindow(object):
 
     def play_pause(self):
         if self.is_started:
+
             if self.is_playing:
                 self.is_playing = False
                 self.play_pause_btn.setText('Click to resume')
-                self.game_widget.data_to_draw(Circle()) #
+                self.game_widget.data_to_draw(Circle())
             else:
                 self.is_playing = True
                 self.play_pause_btn.setText('Click to stop')
-                self.game_widget.clear() #
+                self.game_widget.clear()
+
         else:
             self.is_started = True
             self.is_playing = True
             self.play_pause_btn.setText('Click to stop')
 
 
+class Grid(QtWidgets.QWidget):
 
-class Board(QtWidgets.QWidget):
+    def __init__(self, widget):
+        super().__init__(widget)
+        self.x_blocks = 50
+        self.y_blocks = 50
+        self.x_size = ImageWidget.size[0] // self.x_blocks
+        self.y_size = ImageWidget.size[1] // self.y_blocks
+    
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+
+        for row in range(self.x_blocks+1):
+            painter.drawLine(row*self.x_size, 0, row*self.x_size, self.y_blocks*self.y_size)
+        
+        for column in range(self.y_blocks+1):
+            painter.drawLine(0, column*self.y_size, self.x_blocks*self.x_size, column*self.y_size)
+
+    
+class ImageWidget(QtWidgets.QWidget):
+
+    size = (800, 800)
     
     def __init__(self, widget):
         super().__init__(widget)
@@ -61,7 +86,40 @@ class Board(QtWidgets.QWidget):
         for object in self.data:
             object.draw(painter)
 
+
+class Cell:
+    '''
+    in progress
+
+    directions:
+    1 2 3
+    8 0 4
+    7 6 5
+    this is closed by mod 8
+    '''
+
+    directions = {1: (-1, -1), 2: (0, -1), 3: (1, -1), 
+                    8: (-1, 0), 0: (0, 0), 4: (1, 0),
+                    7: (-1, 1), 6: (0, 1), 5: (1, 1)}
+
+    def __init__(self, x, y, dir):
+        self.x = x
+        self.y = y
+        self.trend = dir % 8 #cell view direction
+        self.eyes = ( (self.eye + 1) % 8, self.eye, (self.eye + 7) % 8 ) #directions that cell sees
+        self.vision = [(self.x + Cell.directions[self.eyes[0]][0], self.y + Cell.directions[self.eyes[0]][1]),
+                         (self.x + Cell.directions[self.eyes[1]][0], self.y + Cell.directions[self.eyes[1]][1]),
+                         (self.x + Cell.directions[self.eyes[2]][0], self.y + Cell.directions[self.eyes[2]][1])] #coordinates that cell sees : [(x1,y1), (x2,y2), (x3,y3)]
+    
+    def move(self):
+        pass
+
+        
+
+
+
 class Circle:
+
     def __init__(self):
         self.x = 100
         self.y = 100
