202cccb7ff92f845d4cf57374beff7687bff711e
VitaliyDzhabrailov <vitalyadd22000022@gmail.com>
2023-05-23 16:25:00 +0300

    Painter v2.0

--- supporting and old/Timer.py
+++ supporting and old/Timer.py
@@ -1,70 +1,28 @@
-import sys
-from PyQt6.QtCore import Qt, QTimer
-from PyQt6.QtWidgets import QWidget, QApplication, \
-    QLCDNumber, QSlider, QVBoxLayout, QHBoxLayout, QPushButton
+import numpy as np
 
-
-class MainWindow(QWidget):
-
+class Field:
     def __init__(self):
-        super().__init__()
+        self.field = np.zeros(2704)
+        self.field = self.field.reshape(52, 52)
+        # 0 - floor, 1 - wall, 2 - herbivore, 3 - predator
+        self.field[0, :] += 1
+        self.field[:, 0] += 1
+        self.field[-1, :] += 1
+        self.field[:, -1] += 1
+        print('field init')
 
-        self.lcd = QLCDNumber(self)
-        # Устанавливаем значение по умолчанию на дисплей
-        self.lcd.display(1)
+    def cell_step(self, type, x_start, y_start, x_finish, y_finish):
+        self.field[x_start, y_start] = 0
+        self.field[x_finish, y_finish] = type
 
-        self.slider = QSlider(Qt.Orientation.Horizontal, self)
-        # Устанавливаем минимальное и максимальное значение
-        self.slider.setMinimum(1)
-        self.slider.setMaximum(90)
-        self.slider.valueChanged.connect(self.lcd.display)
-
-        self.start_btn = QPushButton('Start', self)
-        self.start_btn.clicked.connect(self.start_btn_clicked)
-        self.toggle_btns()
-
-        hbox = QHBoxLayout()
-        hbox.addWidget(self.slider)
-        hbox.addWidget(self.start_btn)
-
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.lcd)
-        vbox.addLayout(hbox)
-
-        self.setLayout(vbox)
-
-        self.setWindowTitle('Timer PyQt5')
-        self.resize(400, 300)
-
-    def toggle_btns(self, value=True):
-        self.slider.setEnabled(value)
-        self.start_btn.setEnabled(value)
-
-    def start_btn_clicked(self):
-        # Отключаем слайдер и кнопку старта
-        self.toggle_btns(False)
-        # запускаем отсчет
-        self.tick_timer()
-
-    def tick_timer(self):
-        # Получаем значение на LCD виджете
-        lcd_value = self.lcd.value()
-        if lcd_value > 0:
-            # Устанавливаем значение на 1 меньше
-            self.lcd.display(lcd_value - 1)
-            # Засекаем таймер - значение в милисекундах
-            # метод singleShot создает поток в фоне, отменить его нельзя
-            QTimer().singleShot(1000, self.tick_timer)
-        else:
-            # Значение дисплея стало 0
-            # Включаем элементы интерфейса обратно
-            self.toggle_btns()
-            # Устанавливаем на дисплей выбранную на слайдере настройку
-            self.lcd.display(self.slider.value())
+    def get_info(self):
+        return self.field
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+a = Field()
+b = a.get_info()
+print(type(b))
+print(len(b))
+for x in range(len(b)):
+    for y in range(len(b)):
+        print(x, y, b[x][y])
