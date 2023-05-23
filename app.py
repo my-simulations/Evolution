7f3a3ce5db585619942c718222d002b6d434af7f
VitaliyDzhabrailov <vitalyadd22000022@gmail.com>
2023-05-23 19:01:43 +0300

    constructor

--- app.py
+++ app.py
@@ -21,10 +21,9 @@ class MyWindow(object):
         self.game_widget = ImageWidget(MainWindow)
         self.game_widget.setGeometry(QtCore.QRect(0, 0, ImageWidget.size[0]+1, ImageWidget.size[1]+1))
 
-
-
         self.play_pause_btn = QtWidgets.QPushButton(MainWindow)
         self.play_pause_btn.setGeometry(QtCore.QRect(866, 670, 200, 80))
+
         self.play_pause_btn.setText('Start')
         self.play_pause_btn.pressed.connect(self.play_pause)
 
@@ -35,6 +34,16 @@ class MyWindow(object):
 
         self.tick_duration = 1000
 
+        self.game_field = Field()
+        self.list_of_cells = []
+        for i in range(8):
+            self.list_of_cells.append(Herbivore(randint(1, 50), randint(1, 50), randint(0, 7), randint(5, 30)))
+        for i in range(2):
+            self.list_of_cells.append(Predator(randint(1, 50), randint(1, 50), randint(0, 7), randint(5, 30)))
+
+        for i in self.list_of_cells:
+            self.game_field.cell_born(i.type, i.x, i.y)
+
 
     def play_pause(self):
         if self.is_started:
@@ -43,19 +52,33 @@ class MyWindow(object):
                 self.is_playing = False
                 self.play_pause_btn.setText('Click to resume')
                 self.game_widget.timer.stop()
+                self.mega_think(0)
             else:
                 self.is_playing = True
                 self.play_pause_btn.setText('Click to stop')
                 self.game_widget.timer.start(self.tick_duration)
+                self.mega_think(1)
 
         else:
             self.is_started = True
             self.is_playing = True
             self.play_pause_btn.setText('Click to stop')
             self.game_widget.timer.start(self.tick_duration)
+            self.mega_think(1)
 
-
-
+    def mega_think(self, flag):
+        while flag:
+            for i in self.list_of_cells:
+                decisions = i.think()
+                for j in decisions:
+                    if j == False:
+                        pass
+                    elif j[0] == 'born':
+                        self.game_field.cell_born(j[1], j[2], j[3])
+                    elif j[0] == 'move' or j[0] == 'eat':
+                        self.game_field.cell_step(j[1], j[2], j[3], j[4], j[5])
+                    else:
+                        pass
 
 class Grid(QtWidgets.QWidget):
 
@@ -88,7 +111,7 @@ class ImageWidget(QtWidgets.QWidget):
         super().__init__(widget)
 
         self.data = []
-        self.game_field = Field()
+
 
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.tick)
@@ -106,15 +129,15 @@ class ImageWidget(QtWidgets.QWidget):
 
         print(self.time)
 
-        self.game_field.cell_step(2, a, b, self.x, self.y)
-        self.game_field.cell_step(3, a + 3, b, self.x + 3, self.y)
+        ui.game_field.cell_step(2, a, b, self.x, self.y)
+        ui.game_field.cell_step(3, a + 3, b, self.x + 3, self.y)
         self.clear()
         self.data_to_draw()
 
         print(self.x)
     
     def data_to_draw(self):
-        self.data = self.game_field.get_info()
+        self.data = ui.game_field.get_info()
         self.update()
     
     def clear(self):
@@ -178,6 +201,9 @@ class Field:
 
     def cell_step(self, type, x_start, y_start, x_finish, y_finish):
         self.field[x_start, y_start] = 0
+        self.field[x_finish, y_finish] = type
+
+    def cell_born(self, type, x_finish, y_finish):
         self.field[x_finish, y_finish] = type
 
     def get_info(self):
