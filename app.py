04ea9b7f6ee58a7356e33473f5c3fa59afc1e3ef
VitaliyDzhabrailov <vitalyadd22000022@gmail.com>
2023-05-23 16:25:09 +0300

    Painter v2.0

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class MyWindow(object):
 
     def __init__(self, MainWindow):
         MainWindow.setWindowTitle('Evolution')
-        MainWindow.resize(1100, 800)
+        MainWindow.resize(1100, 850)
 
         self.central_widget = QtWidgets.QWidget(MainWindow)
 
@@ -19,6 +19,8 @@ class MyWindow(object):
 
         self.game_widget = ImageWidget(MainWindow)
         self.game_widget.setGeometry(QtCore.QRect(0, 0, ImageWidget.size[0]+1, ImageWidget.size[1]+1))
+
+
 
         self.play_pause_btn = QtWidgets.QPushButton(MainWindow)
         self.play_pause_btn.setGeometry(QtCore.QRect(850, 670, 200, 80))
@@ -59,8 +61,8 @@ class Grid(QtWidgets.QWidget):
     def __init__(self, widget):
         super().__init__(widget)
 
-        self.x_blocks = 50
-        self.y_blocks = 50
+        self.x_blocks = 52
+        self.y_blocks = 52
         self.x_size = ImageWidget.size[0] // self.x_blocks
         self.y_size = ImageWidget.size[1] // self.y_blocks
 
@@ -79,29 +81,36 @@ class Grid(QtWidgets.QWidget):
     
 class ImageWidget(QtWidgets.QWidget):
 
-    size = (800, 800)
+    size = (832, 832)
     
     def __init__(self, widget):
         super().__init__(widget)
-        self.data=[]
+
+        self.data = []
+        self.game_field = Field()
 
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.tick)
 
-        self.x = 100
-        self.y = 100
+        self.time = 0
+        self.x = 1
+        self.y = 1
 
     def tick(self):
-        self.x += 10
-        self.y += 10
+        self.x += 1
+        self.y += 1
+        self.time += 1
 
+        print(self.time)
+
+        self.game_field.cell_step(2, self.x - 1, self.y - 1, self.x, self.y)
         self.clear()
-        self.data_to_draw(Circle(self.x, self.y))
+        self.data_to_draw()
 
         print(self.x)
     
-    def data_to_draw(self, data):
-        self.data.append(data)
+    def data_to_draw(self):
+        self.data = self.game_field.get_info()
         self.update()
     
     def clear(self):
@@ -110,25 +119,66 @@ class ImageWidget(QtWidgets.QWidget):
 
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
-        for object in self.data:
-            object.draw(painter)
+        for x in range(len(self.data)):
+            for y in range(len(self.data)):
+                # if self.data[x][y] == 0:
+                #     Square(x, y).draw(painter, 0x009)
+                if self.data[x][y] == 1:
+                    Square(x, y).draw(painter, 0x474747)
+                elif self.data[x][y] == 2:
+                    Circle(x, y, 3).draw(painter, 0x090)
+                elif self.data[x][y] == 3:
+                    Circle(x, y, 3).draw(painter, 0x474747)
 
+
+
+
+class Square:
+    def __init__(self, x=100, y=100):
+        self.x = x * 16
+        self.y = y * 16
+
+    def draw(self, painter, color=0x474747):
+        painter.setPen(QtGui.QColor(color))
+        painter.setBrush(QtGui.QColor(color))
+        painter.drawRect(self.x, self.y, 14, 14)
 
 class Circle:
 
-    def __init__(self, x=100, y=100):
-        self.x = x
-        self.y = y
+    def __init__(self, x, y, direction):
+        self.x = x * 16 + 2
+        self.y = y * 16 + 2
+        self.direction = direction
     
     def draw(self, painter, color=0x474747):
         painter.setPen(QtGui.QColor(color))
         painter.setBrush(QtGui.QColor(color))
-        painter.drawEllipse(self.x, self.y, 15, 15)
+        painter.drawEllipse(self.x, self.y, 12, 12)
+
+        painter.setPen(QtGui.QColor(0x009))
+        painter.setBrush(QtGui.QColor(0x009))
+        painter.drawLine(self.x+6, self.y+6,
+                         self.x+6 + round(6*np.cos(np.pi/8*self.direction)), self.y+6 + round(6*np.sin(np.pi/8*self.direction)))
 
 
-class Floor(): # тут что-то про ограничение коробки
-    field = np.zeros()
-    field.reshape(50, 50)
+
+class Field:
+    def __init__(self):
+        self.field = np.zeros(2704)
+        self.field = self.field.reshape(52, 52)
+        # 0 - floor, 1 - wall, 2 - herbivore, 3 - predator
+        self.field[0, :] = np.array([1]*52)
+        self.field[:, 0] = np.array([1]*52)
+        self.field[-1, :] = np.array([1]*52)
+        self.field[:, -1] = np.array([1]*52)
+
+    def cell_step(self, type, x_start, y_start, x_finish, y_finish):
+        self.field[x_start, y_start] = 0
+        self.field[x_finish, y_finish] = type
+
+    def get_info(self):
+        return self.field
+
 
 
 
