b5b7bdce64c97ce7a202c7144f28e9688d63d386
Square596 <artem211201@gmail.com>
2023-05-23 03:23:38 +0300

    pyqt6, class 'Cell' and its methods

--- v2.py
+++ v2.py
@@ -12,8 +12,7 @@ class MyWindow(object):
 
         self.grid = Grid(MainWindow)
         self.grid.setGeometry(QtCore.QRect(0, 0, ImageWidget.size[0]+1, ImageWidget.size[1]+1))
-        # self.grid.setAttribute(QtCore.Qt.WA_StyledBackground)
-        self.grid.setStyleSheet('background-color: rgb(134, 230, 141);')
+        # self.grid.setStyleSheet('background-color: rgb(134, 230, 141);')
 
         self.game_widget = ImageWidget(MainWindow)
         self.game_widget.setGeometry(QtCore.QRect(0, 0, ImageWidget.size[0]+1, ImageWidget.size[1]+1))
@@ -34,16 +33,17 @@ class MyWindow(object):
             if self.is_playing:
                 self.is_playing = False
                 self.play_pause_btn.setText('Click to resume')
-                self.game_widget.data_to_draw(Circle())
+                self.game_widget.clear()
             else:
                 self.is_playing = True
                 self.play_pause_btn.setText('Click to stop')
-                self.game_widget.clear()
+                self.game_widget.data_to_draw(Circle())
 
         else:
             self.is_started = True
             self.is_playing = True
             self.play_pause_btn.setText('Click to stop')
+            self.game_widget.data_to_draw(Circle())
 
 
 class Grid(QtWidgets.QWidget):
@@ -89,8 +89,6 @@ class ImageWidget(QtWidgets.QWidget):
 
 class Cell:
     '''
-    in progress
-
     directions:
     1 2 3
     0 8 4
@@ -108,33 +106,70 @@ class Cell:
         self.x = x
         self.y = y
         self.trend = dir % 8 #cell view direction
-        self.eyes = ((self.trend + 1) % 8, self.trend, (self.trend + 7) % 8) #directions that cell sees
+        self.get_vision_from_direction(self.trend)
+        
+    def get_vision_from_direction(self, main_direction):
+        self.eyes = ((main_direction + 1) % 8, main_direction % 8, (main_direction + 7) % 8) #directions that cell sees
         self.vision = [(self.x + Cell.directions[self.eyes[0]][0], self.y + Cell.directions[self.eyes[0]][1]),
                          (self.x + Cell.directions[self.eyes[1]][0], self.y + Cell.directions[self.eyes[1]][1]),
                          (self.x + Cell.directions[self.eyes[2]][0], self.y + Cell.directions[self.eyes[2]][1])] #coordinates that cell sees : [(x1,y1), (x2,y2), (x3,y3)]
     
-    def move(self):
-        self.energy -= 1
-        self.x = self.vision[1][0]
-        self.y = self.vision[1][1]
-
-
+    def get_objects_for_vision(self, objects):
+        self.near_objects = [objects[self.vision[0][0]][self.vision[0][1]],
+                             objects[self.vision[1][0]][self.vision[1][1]],
+                             objects[self.vision[2][0]][self.vision[2][1]]]
+    
+    def action(self):
+        return self.x, self.y, self.trend
 
     def rotate(self, direction):
-        self.trend = direction % 8  # cell view direction
-        self.eyes = ((self.trend + 1) % 8, self.trend, (self.trend + 7) % 8)  # directions that cell sees
-        self.vision = [(self.x + Cell.directions[self.eyes[0]][0], self.y + Cell.directions[self.eyes[0]][1]),
-                       (self.x + Cell.directions[self.eyes[1]][0], self.y + Cell.directions[self.eyes[1]][1]),
-                       (self.x + Cell.directions[self.eyes[2]][0], self.y + Cell.directions[self.eyes[2]][1])]  # coordinates that cell sees : [(x1,y1), (x2,y2), (x3,y3)]
+        self.trend = direction % 8
+        self.get_vision_from_direction(self.trend)
+        self.action()
+    
+    def reproduction(self):
+        '''in progress'''
+        pass
 
 
+class Herbivore(Cell):
+
+    type = 'herbivore'
+
+    def __init__(self, x, y, dir, energy):
+        super().__init__(x, y, dir, Herbivore.type, energy)
+    
     def photosynthesis(self):
         self.energy += 1
+        self.action()
+
+    def move(self, idx_to_move):
+        if isinstance(self.near_objects[idx_to_move], (Predator, Herbivore)):
+            self.photosynthesis()
+        else:
+            self.x = self.vision[idx_to_move][0]
+            self.y = self.vision[idx_to_move][1]
+            self.action()
 
 
-    def eat(self, other_cell):
-        self.energy += other_cell.energy
+class Predator(Cell):
 
+    type = 'predator'
+
+    def __init__(self, x, y, dir, energy):
+        super().__init__(x, y, dir, Predator.type, energy)
+    
+    def move(self, idx_to_move: int):
+        '''idx_to_move = 0, 1, 2 - index of self.vision'''
+        if isinstance(self.near_objects[idx_to_move], (Predator, Herbivore)):
+            self.energy += 1
+        else:
+            self.energy -= 1
+        
+        self.x = self.vision[idx_to_move][0]
+        self.y = self.vision[idx_to_move][1]
+
+        self.action()
 
 
 class Floor(): # тут что-то про ограничение коробки
